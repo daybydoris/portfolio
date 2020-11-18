32b1b69f127555b7d529b909a25cabee3bcc0248
graphicnovel <onegoseen@gmail.com>
2020-11-18 16:43:22 +0900

    [add] project-detail.js
    - 이전 게시글, 다음 게시글로 이동하는 기능 추가
    - json 데이터 받아와서 html 태그로 뿌려줌
    - 중점 구현 기능에 tab 이동 이벤트 추가

--- js/project-detail.js
+++ js/project-detail.js
@@ -13,132 +13,191 @@ $(function () {
             const topTime = document.querySelector('.top-time');
             const demo = document.querySelector('.demo');
             const overall = document.querySelector('.top-overall');
+            const feature = document.querySelector('.feature');
+
+            //prev/next
+            const prev = document.querySelector('.prev');
+            const next = document.querySelector('.next');
+
+            prev.addEventListener('click', funPrevNext);
+            next.addEventListener('click', funPrevNext);
+
+            function funPrevNext(e){
+                e.preventDefault();
+                let numNow = localStorage.getItem('num');
+
+                if(e.target.closest('div').classList.contains('prev')){
+                    if(numNow > 0){
+                        numNow--;
+                        localStorage.setItem('num', numNow);
+                        location.href = 'project-detail.html';
+                    }else{
+                        alert('첫 게시글입니다.');
+                    }
+                }else{
+                    console.log(numNow);
+                    if(numNow < data.project.length-1){
+                        numNow++;
+                        localStorage.setItem('num', numNow);
+                        location.href = 'project-detail.html';
+                    }else{
+                        alert('마지막 게시글입니다.');
+                    }
+                }
+            }
+
+            
 
             //키 설정 해야함 (localStorage로 받아서)
-            data.project.forEach(function (p, k) {
+            data.project.forEach(function (p) {
+                if(p.num == localStorage.getItem('num')){
 
-                createIndexTxt();
-                projectName();
-                projectTime();
-                createDemo();
-                createIntro();
-                createPagePrev();
-                createUsedSkill();
-                createRes();
+                    createIndexTxt();
+                    projectName();
+                    projectTime();
+                    createDemo();
+                    createIntro();
+                    createPagePrev();
+                    createUsedSkill();
+                    createRes();
+                    createFeature();
 
-                function createIndexTxt() {
-                    const front = document.createElement('div');
-                    const txtBack = document.createElement('span');
+                    function createIndexTxt() {
+                        const front = document.createElement('div');
+                        const txtBack = document.createElement('span');
 
-                    txtBack.textContent = p.title + ".";
-                    txtBack.className = "txt-back";
+                        txtBack.textContent = p.title + ".";
+                        txtBack.className = "txt-back";
 
-                    front.textContent = p.title + ".";
-                    front.className = "front";
+                        front.textContent = p.title + ".";
+                        front.className = "front";
 
-                    front.appendChild(txtBack);
+                        front.appendChild(txtBack);
 
-                    indexTxt.appendChild(front);
-                }
-
-                function projectName() {
-                    const name = document.createElement('p');
-
-                    name.textContent = p.title;
-
-                    topTitle.appendChild(name);
-                }
-
-                function projectTime() {
-                    const time = document.querySelector('p');
-
-                    time.textContent = p.elapsed;
-
-                    topTime.appendChild(time);
-                }
-
-                function createDemo() {
-                    const demoBtn = document.createElement('a');
-                    const circle = document.createElement('a');
-
-                    demoBtn.textContent = "웹사이트 방문하기";
-                    demoBtn.setAttribute("href", p.siteUrl);
-                    demoBtn.setAttribute("target","_blank");
-                    demoBtn.className = "detail-btn atvBtn locate";
-
-                    circle.setAttribute("href", p.siteUrl);
-                    circle.className = "circle atvBtn locate";
-
-                    demo.appendChild(demoBtn);
-                }
-
-                function createIntro() {
-                    const overallDetail = document.createElement('p');
-
-                    overallDetail.innerHTML = p.intro;
-
-                    overall.appendChild(overallDetail);
-                }
-
-                function createPagePrev() {
-                    const pagePrevList = document.querySelector('.page-prev-list');
-
-                    p.prevImg.forEach(function (img, key) {
-                        const pagePrevItem = document.createElement('li');
-                        const pagePrevImg = document.createElement('img');
-
-                        pagePrevImg.setAttribute('src', img);
-                        pagePrevItem.appendChild(pagePrevImg);
-                        pagePrevItem.className = "page-prev-item";
-
-                        pagePrevList.appendChild(pagePrevItem);
-                    });
-                }
-
-                function createUsedSkill() {
-                    const skillList = document.querySelector('.skill-list');
-
-                    p.usedSkill.forEach(function (skill) {
-                        const skillItem = document.createElement('li');
-                        const skillImg = document.createElement('img');
-                        const skillName = document.createElement('span');
-
-                        skillImg.setAttribute('src', `../img/about-icon-${skill}.png`);
-
-                        skillName.textContent = skill;
-
-                        skillItem.className = "skill-item";
-                        skillItem.appendChild(skillImg);
-                        skillItem.appendChild(skillName);
-
-                        skillList.appendChild(skillItem);
-                    });
-                }
-
-                function createRes() {
-                    const resDetail = document.querySelector('.res-detail .inner');
-                    const lastPx = resDetail.querySelector('span');
-
-                    for (var key in p.responsiveType) {
-                        const resolution = document.createElement('span');
-                        const type = document.createElement('span');
-
-                        type.id = key;
-                        type.textContent = key;
-
-                        resolution.textContent = `${p.responsiveType[key]}px`;
-
-                        resDetail.insertBefore(resolution, lastPx);
-                        resDetail.insertBefore(type, lastPx);
+                        indexTxt.appendChild(front);
                     }
 
-                }
+                    function projectName() {
+                        const name = document.createElement('p');
 
+                        name.textContent = p.title;
+
+                        topTitle.appendChild(name);
+                    }
+
+                    function projectTime() {
+                        const time = document.querySelector('p');
+
+                        time.textContent = p.elapsed;
+
+                        topTime.appendChild(time);
+                    }
+
+                    function createDemo() {
+                        const demoBtn = document.createElement('a');
+                        const circle = document.createElement('a');
+
+                        demoBtn.textContent = "웹사이트 방문하기";
+                        demoBtn.setAttribute("href", p.siteUrl);
+                        demoBtn.setAttribute("target","_blank");
+                        demoBtn.classList = "detail-btn atvBtn locate"
+
+                        circle.setAttribute("href", p.siteUrl);
+                        circle.classList = "circle atvBtn locate";
+
+                        demo.appendChild(demoBtn);
+                        demo.appendChild(circle);
+                    }
+
+                    function createIntro() {
+                        const overallDetail = document.createElement('p');
+
+                        overallDetail.innerHTML = p.intro;
+
+                        overall.appendChild(overallDetail);
+                    }
+
+                    function createPagePrev() {
+                        const pagePrevList = document.querySelector('.page-prev-list');
+
+                        p.prevImg.forEach(function (img, key) {
+                            const pagePrevItem = document.createElement('li');
+                            const pagePrevImg = document.createElement('img');
+
+                            pagePrevImg.setAttribute('src', img);
+                            pagePrevItem.appendChild(pagePrevImg);
+                            pagePrevItem.className = "page-prev-item";
+
+                            pagePrevList.appendChild(pagePrevItem);
+                        });
+                    }
+
+                    function createUsedSkill() {
+                        const skillList = document.querySelector('.skill-list');
+
+                        p.usedSkill.forEach(function (skill) {
+                            const skillItem = document.createElement('li');
+                            const skillImg = document.createElement('img');
+                            const skillName = document.createElement('span');
+
+                            skillImg.setAttribute('src', `../img/about-icon-${skill}.png`);
+
+                            skillName.textContent = skill;
+
+                            skillItem.className = "skill-item";
+                            skillItem.appendChild(skillImg);
+                            skillItem.appendChild(skillName);
+
+                            skillList.appendChild(skillItem);
+                        });
+                    }
+
+                    function createRes() {
+                        if(p.isReponsive == true){
+                            const resDetail = document.querySelector('.res-detail .inner');
+                            const lastPx = resDetail.querySelector('span');
+    
+                            for (var key in p.responsiveType) {
+                                const resolution = document.createElement('span');
+                                const type = document.createElement('span');
+    
+                                type.id = key;
+                                type.textContent = key;
+    
+                                resolution.textContent = `${p.responsiveType[key]}px`;
+    
+                                resDetail.insertBefore(resolution, lastPx);
+                                resDetail.insertBefore(type, lastPx);
+                            }
+                        }
+                    }
+
+                    function createFeature(){
+                        const featCon = p.features;
+                        
+                        feature.innerHTML = p.features;
+                    }
+                    
+
+
+                    //중점 구현 기능 Tab 이동
+                    $(".tab a").on("click", function (e) {
+                        e.preventDefault();//anchor이벤트의 기본동작을 막는다.
+                        var thisTarget = $(this).attr("href");
+                        $(window).scrollTop($('#' + thisTarget).offset().top);
+                    });
+
+                }
             });
 
         }
+
+        
+
     });
 
+
+    
 
     //end
 });
