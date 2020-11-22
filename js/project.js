d52652b4e95471f61f36d184e7906e7e8e9d12e6
GraphicNovel <onegoseen@gmail.com>
2020-11-22 18:06:12 +0900

    [add] project page
    - 클릭 이벤트 수정

--- js/project.js
+++ js/project.js
@@ -1,47 +1,68 @@
 "use strict";
 
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function () {
 
-    const projectItem = document.querySelectorAll('.project-item');
+    const projectItem = document.querySelectorAll('.thumb a');
     const thumb = document.querySelectorAll('.thumb');
+    const body = document.querySelector('body');
+    const projectPrev = document.querySelectorAll('.project-prev');
 
-    projectItem.forEach(function(item){
-        item.addEventListener('click', saveProjectId);
-
-        function saveProjectId(e){
-            e.preventDefault();
-            let projectId = e.target.closest('a').id;
-            let projectUrl = e.target.closest('a').href;
-
-            localStorage.setItem('num', projectId);
-
-            location.href = projectUrl;
-        }
-    });
-
-    thumb.forEach(function(t){
+    thumb.forEach(function (t) {
         t.addEventListener('mousemove', thumbMove);
         t.addEventListener('mouseout', thumbStop);
+        t.addEventListener('click', thumbStop);
+        t.addEventListener('click', saveProjectId);
 
-        
-        // -----------------------
+        //-----------------------
+        //게시글 넘버값 저장
+        //-----------------------
+
+        function saveProjectId(e) {
+            e.preventDefault();
+
+            let projectId = e.target.closest('a').id;
+            localStorage.setItem('num', projectId);
+        }
+
+        //-----------------------
+        // 페이지 전환 효과
+        //-----------------------
+
+        //스크롤 막기/허용
+        function stopScroll() {
+            body.classList.add("stopScroll");
+        }
+        function allowScroll() {
+            body.classList.remove("stopScroll");
+        }
+
+
+        //-----------------------
         // 마우스 오버 효과
-        // -----------------------
-        function thumbMove(e){
+        //-----------------------
+
+        function thumbMove(e) {
             let halfW = (this.offsetWidth / 2);
             let halfH = (this.offsetHeight / 2);
 
             let coorX = (halfW - (e.pageX - this.offsetLeft));
             let coorY = (halfH - (e.pageY - this.offsetTop));
 
-            let degX = ((coorY / halfH) * 15 ) + 'deg';
-            let degY = ((coorX / halfW) * -15 ) + 'deg';
+            let degX = ((coorY / halfH) * 15) + 'deg';
+            let degY = ((coorX / halfW) * -15) + 'deg';
 
             this.style = `transform: perspective(1500px) translate3d(0, 0px, 0) rotateX(${degX}) rotateY(${degY})`;
         }
 
-        function thumbStop(){
-            this.style = `transform:rotate(0)`;
+        function thumbStop() {
+            this.style = 'transform:rotate(0)';
         }
+
+        //-----------------------------
+        // 섬네일 클릭 / 페이지 이동 효과
+        //-----------------------------
+
+
+
     });
 });
