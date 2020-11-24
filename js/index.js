37c6ef517f8b69376ca47558bf793f6c279ad47d
GraphicNovel <onegoseen@gmail.com>
2020-11-24 14:59:22 +0900

    [fix] svg 태그 수정
    - slick 내에서 스타일 적용되지 않던 문제 createElementNS를 사용하여 해결
    - locate 클래스를 a태그가 아닌 img-box에 적용

--- js/index.js
+++ js/index.js
@@ -1,95 +1,95 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded',function(){
+window.addEventListener('DOMContentLoaded', function () {
 
     //----------------------------
     // particle.js
     //----------------------------
 
-    particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+    particlesJS.load('particles-js', 'particlesjs-config.json', function () {
         console.log('callback - particles.js config loaded');
-      });
+    });
 
 
     //-----------------------------
     // scrolldown 아이콘 애니메이션
     //-----------------------------
 
-    setInterval(lineActive,2000);
+    setInterval(lineActive, 2000);
 
-    function lineActive(){
+    function lineActive() {
         const lineWrap = document.querySelector('.line-wrap');
         const srcollLine = document.querySelector('.line');
 
-            srcollLine.style = "top:0%; opacity:0.5;";
-        setTimeout(function(){
+        srcollLine.style = "top:0%; opacity:0.5;";
+        setTimeout(function () {
             srcollLine.style = "top:100%";
-        },1000);
-        setTimeout(function(){
+        }, 1000);
+        setTimeout(function () {
             srcollLine.style = "top:100%; opacity:0;";
-        },1400);
-        setTimeout(function(){
+        }, 1400);
+        setTimeout(function () {
             srcollLine.style = "top:-110%; opacity:0;";
-        },1800);
+        }, 1800);
     }
 
-    
+
 
     $.ajax({
         url: "project.json",
         type: "GET",
         success: function (data) {
-            
+
             const slider = document.querySelector('.slider');
 
-            data.project.forEach(function(p){
+            data.project.forEach(function (p) {
 
                 //slick content 넣기
-                    //slider-con
-                    let sliderCon = document.createElement('div');
+                //slider-con
+                let sliderCon = document.createElement('div');
 
-                    //img-box
-                    let imgBox = document.createElement('div');
-                    let aTag = document.createElement('a');
-                    let img = document.createElement('img');
-                    let thumb = p.thumb;
+                //img-box
+                let imgBox = document.createElement('div');
+                let aTag = document.createElement('a');
+                let img = document.createElement('img');
+                let thumb = p.thumb;
 
-                    //svg-box
-                    let svgBox = document.createElement('div');
-                    let svg = document.createElement('svg');
-                    let text = document.createElement('text');
+                //svg-box
+                let svgBox = document.createElement('div');
+                let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+                let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
 
-                    imgBox.className = "img-box";
-                    img.setAttribute('src',thumb);
+                imgBox.className = "img-box";
+                img.setAttribute('src', thumb);
 
-                    aTag.id = p.num;
-                    aTag.setAttribute('href','/portfolio/pages/project-detail.html');
-                    aTag.classList.add('locate');
-                    aTag.classList.add('atvBtn');
-                    aTag.appendChild(img);
+                aTag.id = p.num;
+                aTag.setAttribute('href', '/portfolio/pages/project-detail.html');
+                aTag.classList.add('atvBtn');
+                aTag.appendChild(img);
 
-                    imgBox.appendChild(aTag);
+                imgBox.classList.add('locate');
+                imgBox.appendChild(aTag);
 
-                    text.setAttribute('x','50%');
-                    text.setAttribute('y','50%');
-                    text.setAttribute('text-anchor','middle');
-                    text.textContent = `${p.title}.`;
-                    text.classList = "title";
+                text.setAttribute('x', '50%');
+                text.setAttribute('y', '80%');
+                text.setAttribute('text-anchor', 'middle');
+                text.textContent = `${p.title}.`;
+                text.classList = "title";
 
-                    svg.setAttribute('width','100%');
-                    svg.setAttribute('height','120');
-                    svg.setAttribute('viewbox','0 0 100% 120');
+                svg.setAttribute('width', '100%');
+                svg.setAttribute('height', '120');
+                svg.setAttribute('viewbox', '0 0 100% 120');
 
-                    svgBox.className = "svg-box";
+                svgBox.className = "svg-box";
 
-                    svg.appendChild(text);
-                    svgBox.appendChild(svg);
+                svg.appendChild(text);
+                svgBox.appendChild(svg);
 
-                    sliderCon.className = "slider-con";
-                    sliderCon.appendChild(imgBox);
-                    sliderCon.appendChild(svgBox);
+                sliderCon.className = "slider-con";
+                sliderCon.appendChild(imgBox);
+                sliderCon.appendChild(svgBox);
 
-                    slider.appendChild(sliderCon);
+                slider.appendChild(sliderCon);
 
             });
 
@@ -109,15 +109,16 @@ window.addEventListener('DOMContentLoaded',function(){
             //-----------------------------
             // slick
             //-----------------------------
-            
+
             $(".slider").slick({
                 slidesToShow: 1,
                 autoplay: true,
-                autoplaySpeed: 2000,
+                autoplaySpeed: 3000,
                 centerMode: true,
                 arrows: false,
                 rtl: false,
-                variableWidth: true
+                draggable: true
+                // variableWidth: true
             });
 
         }
