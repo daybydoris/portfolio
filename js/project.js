e698a800117cc295faf0375102d0d5d115fa54ee
GraphicNovel <onegoseen@gmail.com>
2020-11-25 16:40:56 +0900

    [fix] 함수 위치 조정
    - 함수들을 ajax 안으로 옮김

--- js/project.js
+++ js/project.js
@@ -84,9 +84,9 @@ window.addEventListener('DOMContentLoaded', function () {
                 setTimeout(function () {
 
                     if (p.isResponsive == false) {
-                        pc.style.color = "#545454";
-                        tablet.style.color = "#545454";
-                        mobile.style.color = "#545454";
+                        pc.style.color = "#202020";
+                        tablet.style.color = "#202020";
+                        mobile.style.color = "#202020";
                     } else {
                         pc.style.color = "#99785F";
                         tablet.style.color = "#99785F";
@@ -122,69 +122,72 @@ window.addEventListener('DOMContentLoaded', function () {
 
             });
 
-        }
+            setTimeout(function () {
+                const projectItem = document.querySelectorAll('.thumb a');
+                const thumb = document.querySelectorAll('.thumb');
+                const body = document.querySelector('body');
+                const projectPrev = document.querySelectorAll('.project-prev');
 
+                thumb.forEach(function (t) {
+                    t.addEventListener('mousemove', thumbMove);
+                    t.addEventListener('mouseout', thumbStop);
+                    t.addEventListener('click', thumbStop);
+                    t.addEventListener('click', saveProjectId);
+
+                    //-----------------------
+                    //게시글 넘버값 저장
+                    //-----------------------
+
+                    function saveProjectId(e) {
+                        e.preventDefault();
+
+                        let projectId = e.target.closest('a').id;
+                        localStorage.setItem('num', projectId);
+                    }
+
+                    //-----------------------
+                    // 페이지 전환 효과
+                    //-----------------------
+
+                    //스크롤 막기/허용
+                    function stopScroll() {
+                        body.classList.add("stopScroll");
+                    }
+                    function allowScroll() {
+                        body.classList.remove("stopScroll");
+                    }
+
+
+                    //-----------------------
+                    // 마우스 오버 효과
+                    //-----------------------
+
+                    function thumbMove(e) {
+                        let halfW = (this.offsetWidth / 2);
+                        let halfH = (this.offsetHeight / 2);
+
+                        let coorX = (halfW - (e.pageX - this.offsetLeft));
+                        let coorY = (halfH - (e.pageY - this.offsetTop));
+
+                        let degX = ((coorY / halfH) * 15) + 'deg';
+                        let degY = ((coorX / halfW) * -15) + 'deg';
+
+                        this.style = `transform: perspective(1500px) translate3d(0, 0px, 0) rotateX(${degX}) rotateY(${degY})`;
+                    }
+
+                    function thumbStop() {
+                        this.style = 'transform:rotate(0)';
+                    }
+
+
+
+                });
+            }, 500);
+
+        }
 
 
     });
 
-    const projectItem = document.querySelectorAll('.thumb a');
-    const thumb = document.querySelectorAll('.thumb');
-    const body = document.querySelector('body');
-    const projectPrev = document.querySelectorAll('.project-prev');
 
-    thumb.forEach(function (t) {
-        t.addEventListener('mousemove', thumbMove);
-        t.addEventListener('mouseout', thumbStop);
-        t.addEventListener('click', thumbStop);
-        t.addEventListener('click', saveProjectId);
-
-        //-----------------------
-        //게시글 넘버값 저장
-        //-----------------------
-
-        function saveProjectId(e) {
-            e.preventDefault();
-
-            let projectId = e.target.closest('a').id;
-            localStorage.setItem('num', projectId);
-        }
-
-        //-----------------------
-        // 페이지 전환 효과
-        //-----------------------
-
-        //스크롤 막기/허용
-        function stopScroll() {
-            body.classList.add("stopScroll");
-        }
-        function allowScroll() {
-            body.classList.remove("stopScroll");
-        }
-
-
-        //-----------------------
-        // 마우스 오버 효과
-        //-----------------------
-
-        function thumbMove(e) {
-            let halfW = (this.offsetWidth / 2);
-            let halfH = (this.offsetHeight / 2);
-
-            let coorX = (halfW - (e.pageX - this.offsetLeft));
-            let coorY = (halfH - (e.pageY - this.offsetTop));
-
-            let degX = ((coorY / halfH) * 15) + 'deg';
-            let degY = ((coorX / halfW) * -15) + 'deg';
-
-            this.style = `transform: perspective(1500px) translate3d(0, 0px, 0) rotateX(${degX}) rotateY(${degY})`;
-        }
-
-        function thumbStop() {
-            this.style = 'transform:rotate(0)';
-        }
-
-
-
-    });
 });
