0446194854c65f2aba1210654a10ea2706c6d81b
GraphicNovel <onegoseen@gmail.com>
2020-11-25 16:01:24 +0900

    [update] project 반응형
    - project 페이지 mobile, tablet 반응형 작업

--- js/project.js
+++ js/project.js
@@ -2,6 +2,132 @@
 
 window.addEventListener('DOMContentLoaded', function () {
 
+
+    $.ajax({
+        url: "../project.json",
+        type: "GET",
+        success: function (data) {
+
+            data.project.forEach(function (p, key) {
+
+                //createElement
+
+                let projectList = document.querySelector('.project-list'),
+                    projectItem = document.createElement('li'),
+
+                    thumb = document.createElement('div'),
+                    aTag = document.createElement('a'),
+                    thumbImg = document.createElement('img'),
+
+                    projectPrev = document.createElement('div'),
+                    svgBox = document.createElement('div'),
+                    titleSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
+
+                    responsive = document.createElement('div'),
+
+                    prevText = document.createElement('p');
+
+                // classList 및 setAttribute
+
+                //thumb
+                thumbImg.setAttribute('src', `../${p.thumb}`);
+
+                aTag.href = p.siteUrl;
+                aTag.id = key;
+                aTag.classList = "atvBtn locate";
+
+                thumb.classList = "thumb";
+
+                aTag.appendChild(thumbImg);
+                thumb.appendChild(aTag);
+
+                //svgBox
+                titleSvg.innerHTML = p.mobileTitle + p.pcTitle;
+                titleSvg.classList = "title-svg";
+
+                svgBox.classList = "svg-box";
+
+                projectPrev.classList = "project-prev";
+
+                svgBox.appendChild(titleSvg);
+                projectPrev.appendChild(svgBox);
+
+                //responsive
+
+                let pc = document.createElement('p'),
+                    tablet = document.createElement('p'),
+                    mobile = document.createElement('p'),
+                    slash1 = document.createElement('span'),
+                    slash2 = document.createElement('span');
+
+                pc.id = "pc",
+                    tablet.id = "tablet",
+                    mobile.id = "mobile";
+
+                pc.textContent = "PC";
+                tablet.textContent = "Tablet";
+                mobile.textContent = "Mobile";
+                slash1.textContent = "/";
+                slash2.textContent = "/";
+
+                responsive.appendChild(pc);
+                responsive.appendChild(slash1);
+                responsive.appendChild(tablet);
+                responsive.appendChild(slash2);
+                responsive.appendChild(mobile);
+
+                responsive.classList = "responsive";
+
+                projectPrev.appendChild(responsive);
+
+
+                setTimeout(function () {
+
+                    if (p.isResponsive == false) {
+                        pc.style.color = "#545454";
+                        tablet.style.color = "#545454";
+                        mobile.style.color = "#545454";
+                    } else {
+                        pc.style.color = "#99785F";
+                        tablet.style.color = "#99785F";
+                        mobile.style.color = "#99785F";
+                    }
+                }, 500);
+
+                //prev-text
+
+                prevText.textContent = p.prevTxt;
+                prevText.classList = "prev-txt";
+                projectPrev.appendChild(prevText);
+
+                function isOdd(num) { return num % 2; }
+
+                if (isOdd(key) == 0) {
+                    projectItem.classList = "project-item odd";
+                } else {
+                    projectItem.classList = "project-item even";
+                }
+                projectItem.appendChild(thumb);
+                projectItem.appendChild(projectPrev);
+                projectItem.setAttribute('data-aos', 'fade-up');
+
+                if (key == 0) {
+                    projectItem.setAttribute('data-aos-delay', '500');
+                } else {
+                    projectItem.setAttribute('data-aos-offset', '500');
+                }
+
+                projectList.appendChild(projectItem);
+
+
+            });
+
+        }
+
+
+
+    });
+
     const projectItem = document.querySelectorAll('.thumb a');
     const thumb = document.querySelectorAll('.thumb');
     const body = document.querySelector('body');
